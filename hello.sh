efd4fde0d8f5efc80f12976802bcd2b826069af2
Owner <owner@OwnernoiMac.local>
2023-09-14 00:32:37 +0900

    second commit

--- hello.sh
+++ hello.sh
@@ -2,3 +2,10 @@ colors=(red blue pink)
 echo ${colors[0]}
 echo ${colors[1]}
 echo ${colors[2]}
+
+# 要素変更をする場合
+colors[2]=silver
+echo ${colors[2]}
+
+# 全要素を出力する場合
+echo ${colors[@]}
---
+++ if.sh
@@ -0,0 +1,7 @@
+# 引数を受け取る
+read -p "Name? " name
+
+ if test "$name" = "taguchi"
+ then
+    echo "welcom"
+fi
